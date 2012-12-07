6640212bf54718c6f26b8130cc549bf59edea1f3
sharath <fbmera@gmail.com>
2012-12-07 16:59:13 -0500

    removing invitaiton per user

--- gateways/DARE-CACTUS/apps/invitation/admin.py
+++ gateways/DARE-CACTUS/apps/invitation/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from invitation.models import InvitationKey, InvitationUser
+from invitation.models import InvitationKey
+
 
 class InvitationKeyAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'from_user', 'date_invited', 'key_expired')
 
-class InvitationUserAdmin(admin.ModelAdmin):
-    list_display = ('inviter', 'invitations_remaining')
-
 admin.site.register(InvitationKey, InvitationKeyAdmin)
-admin.site.register(InvitationUser, InvitationUserAdmin)
