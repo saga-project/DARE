501acbbd54347870681e850de6f30c981213bd10
sharath <fbmera@gmail.com>
2013-01-18 02:23:21 -0500

    updated task save anim

--- gateways/DARE-BIGJOB/site_media/assets/js/base.js
+++ gateways/DARE-BIGJOB/site_media/assets/js/base.js
@@ -27,3 +27,22 @@ $("a[data-toggle=deletetask]").click(function(){
         });
     });
 });
+
+
+var opts = {
+  lines: 15, // The number of lines to draw
+  length: 21, // The length of each line
+  width: 5, // The line thickness
+  radius: 25, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 83, // The rotation offset
+  color: '#000', // #rgb or #rrggbb
+  speed: 2.1, // Rounds per second
+  trail: 85, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: 'auto', // Top position relative to parent in px
+  left: 'auto' // Left position relative to parent in px
+};
