3ee3dabc51ee94ab10138d4ca9200f1fe33c7660
sharath <fbmera@gmail.com>
2013-06-02 20:14:29 -0400

    minor fixes

--- DARE-BIGJOB/site_media/assets/js/base.js
+++ DARE-BIGJOB/site_media/assets/js/base.js
@@ -1,10 +1,24 @@
+show_hide_cores =  function() {
+    if ($("#pilotPopupSelect option:selected").attr('type-pilot') =='osg'){
+        $('#id_cores').parent().parent().hide();
 
+    } else{
+        $('#id_cores').parent().parent().show();
+        $('#id_cores').val($("#pilotPopupSelect option:selected").attr('default-cores'));
+    }
+};
 $(document).ready(function() {
+  show_hide_cores();
 
   $('ul.nav a').filter(function() {
       var url = window.location;
         return (this.href == url.href.split( '?' )[0]);
       }).parent().addClass('active');
+
+
+  $("#pilotPopupSelect").change(function(){
+    show_hide_cores();
+  });
 
   editAreaLoader.init({
     id : "id_script",
